91829a42ca91c9f6e6539ce9a89b347a0b9c3b05
Jacob L <jacob@MacBook-Air-de-Jacob.local>
2023-05-11 19:51:20 -0400

    added func in ex00

--- CPP Module 01/ex00/main.cpp
+++ CPP Module 01/ex00/main.cpp
@@ -6,23 +6,31 @@
 /*   By: jalevesq <jalevesq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 19:21:57 by jalevesq          #+#    #+#             */
-/*   Updated: 2023/05/11 19:38:36 by jalevesq         ###   ########.fr       */
+/*   Updated: 2023/05/11 19:50:45 by jalevesq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Zombie.hpp"
 
+Zombie  *newZombie(std::string name)
+{
+    Zombie *zombie = new Zombie(name);
+    return (zombie);
+}
+
+void randomChump( std::string name )
+{
+    Zombie zombie(name);
+    zombie.announce();
+}
+
 // Manque fonction randomchump. Pas certain de comprendre?
 int main(void)
 {
-    Zombie zombie;
-    Zombie Bob("Bob");
-    Zombie* newZombie = new Zombie("Ethan");
-
-    newZombie->announce();
-    zombie.announce();
-    Bob.announce();
-    delete (newZombie);
+    Zombie *Bob = newZombie("Bob");
+    Bob->announce();
+    randomChump("Roger");
+    delete(Bob);
     return (0);
 }
