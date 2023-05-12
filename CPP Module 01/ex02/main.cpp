5806a52c90693e79b5aefda7db3f1089258f43e7
Jacob L <jacob@MacBook-Air-de-Jacob.local>
2023-05-11 20:19:13 -0400

    ex02 finish

---
+++ CPP Module 01/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalevesq <jalevesq@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/11 20:12:34 by jalevesq          #+#    #+#             */
+/*   Updated: 2023/05/11 20:18:19 by jalevesq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+
+// ** PLUS LIRE SUR CE QU'EST UNE REFERENCE **
+int main(void)
+{
+    std::string string = "HI THIS IS BRAIN";
+    
+    std::string* stringPTR = &string;
+    std::string& stringREF = string;
+
+    std::cout << "Addresse de string: "<< &string << std::endl;
+    std::cout << "Addresse de stringPTR: "<< stringPTR << std::endl;
+    std::cout << "Addresse de stringREF: "<< &stringREF << std::endl;
+    std::cout << std::endl;
+    std::cout << "Contenu de string: "<< string << std::endl;
+    std::cout << "Contenu de stringPTR: "<< *stringPTR << std::endl;
+    std::cout << "Contenu de stringREF: "<< stringREF << std::endl;
+}
