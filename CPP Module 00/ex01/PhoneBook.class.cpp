0dee489bc34591778838705c38eedb6ebbf50e27
Jacob L <jacob@MacBook-Air-de-Jacob.local>
2023-05-13 12:03:06 -0400

    Made board when search

--- CPP Module 00/ex01/PhoneBook.class.cpp
+++ CPP Module 00/ex01/PhoneBook.class.cpp
@@ -6,14 +6,16 @@
 /*   By: jalevesq <jalevesq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 11:17:13 by jalevesq          #+#    #+#             */
-/*   Updated: 2023/05/13 09:08:04 by jalevesq         ###   ########.fr       */
+/*   Updated: 2023/05/13 12:00:34 by jalevesq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <string>
-#include <sstream>
+#include <iomanip>
 #include "PhoneBook.class.hpp"
+
+#define MAX_CONTACTS 8
+#define WIDTH 11
 
 PhoneBook::PhoneBook(void) {
     std::cout << "Constructor Phonebook called" << std::endl;
@@ -27,34 +29,37 @@ PhoneBook::~PhoneBook(void) {
 void    PhoneBook::search(void) {
 
     std::string input;
-    int number;
-    std::cout << "Input an Index between 1 and 8" << std::endl;
-    do {
-        getline(std::cin, input);
-         try {
-            // can't use stoi. is in c++11 not 98
-            number = std::stoi(input);
-            if (number < 1 || number > 8)
-                std::cout << "Invalid Index. Please enter an index between 1 and 8." << std::endl;
-        }
-        catch (const std::exception& e) {
-            std::cout << "Invalid input. Please enter an integer." << std::endl;
-        }
-        } while (number < 1 || number > 8);
-    number -= 1;
-    std::cout << "Name: " << _contacts[number].getName() << std::endl;
-    std::cout << "Last name: " << _contacts[number].getLastName() << std::endl;
-    std::cout << "Nickname: " << _contacts[number].getNickname() << std::endl;
-    std::cout << "Phone numnber: " << _contacts[number].getPhoneNumber() << std::endl;
-    std::cout << "Darkest secret: " << _contacts[number].getSecret() << std::endl;
-}
+    std::cout << std::right;
+    std::cout << std::setw(WIDTH) << "index" << " | "
+              << std::setw(WIDTH) << "Prénom" << " | "
+              << std::setw(WIDTH) << "Nom" << " | "
+              << std::setw(WIDTH) << "Nickname" << std::endl;
 
+    for (int i = 0; i < MAX_CONTACTS; i++) {
+        std::string firstName = _contacts[i].getName().substr(0, WIDTH - 1);
+        if (_contacts[i].getName().length() >= WIDTH)
+            firstName[WIDTH - 2] = '.';
+
+        std::string lastName = _contacts[i].getLastName().substr(0, WIDTH - 1);
+        if (_contacts[i].getLastName().length() >= WIDTH)
+            lastName[WIDTH - 2] = '.';
+
+        std::string nickname =_contacts[i].getNickname().substr(0, WIDTH - 1);
+        if (_contacts[i].getNickname().length() >= WIDTH)
+            nickname[WIDTH - 2] = '.';
+
+
+        std::cout << std::setw(WIDTH) << i + 1 << " | "
+                  << std::setw(WIDTH) << firstName << " | "
+                  << std::setw(WIDTH) << lastName << " | "
+                  << std::setw(WIDTH) << nickname << std::endl;
+    }
+}
 void    PhoneBook::addContact(void) {
     
     std::string info;
     
-    if (_contactNumber > 7)
-        _contactNumber = 0;
+    _contactNumber %= 8;
     // First name
     do {
         std::cout << "Enter the first name of the contact. This field cannot be empty." << std::endl;
