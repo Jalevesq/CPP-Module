375179c0c3344c1f2a3cf3c5a66b04d93ce36ff3
Jacob L <jacob@MacBook-Air-de-Jacob.local>
2023-05-13 20:56:36 -0400

    cleaner

--- CPP Module 01/ex04/main.cpp
+++ CPP Module 01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jalevesq <jalevesq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 15:42:39 by jalevesq          #+#    #+#             */
-/*   Updated: 2023/05/13 20:42:52 by jalevesq         ###   ########.fr       */
+/*   Updated: 2023/05/13 20:56:09 by jalevesq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,29 @@
 #include <string>
 #include <fstream>
 
+
+std::ofstream ft_create_file(std::string output_file)
+{
+    std::ofstream outfile(output_file);
+    if (outfile.is_open() == false)
+    {
+        std::cerr << "Error: Unable to open/create outfile:" << output_file << std::endl;
+        return (std::ofstream());
+    }
+    if (outfile.good() == false)
+    {
+        std::cerr << "Error: Output failed. Not good stream." << std::endl;
+        return (std::ofstream());
+    }
+    return (outfile);
+}
+
 int main(int ac, char **av)
 {
-    std::string output_file;
-    std::string input_file;
-    std::string s1;
-    std::string s2;
+    std::string input_file, output_file;
+    std::string s1, s2;
     std::string line;
-    size_t lastPos;
+    size_t      lastPos;
 
     if (ac != 4)
     {
@@ -30,35 +45,31 @@ int main(int ac, char **av)
     }
     s1 = av[2];
     s2 = av[3];
-    // Try to open file av[1].
+
+    // Ouverture du infile (av[1])
     input_file = av[1];
     std::ifstream infile(input_file);
     if (infile.is_open() == false) {
-        std::cerr << "Error: Unable to open infile" << std::endl;
+        std::cerr << "Error: Unable to open infile: " << input_file << std::endl;
         return (1);
     }
 
-    // Try to create outfile.
+    // Création du outfile avec l'extension .replace
     output_file = input_file + ".replace";
-    std::ofstream outfile(output_file);
-    if (outfile.is_open() == false)
+    std::ofstream outfile = ft_create_file(output_file);
+    if (!outfile)
     {
-        std::cerr << "Error: Unable to open/create outfile." << std::endl;
+        infile.close();
         return (1);
     }
-    if (outfile.good() == false)
-    {
-        std::cerr << "Error: Output failed. Not good stream." << std::endl;
-        return (1);
-    }
-    // Remove and replace string.
+
+    // Remplace chaque occurence s1 par s2 dans outfile.replace
     while (getline(infile, line)) {
        lastPos = 0;
         while ((lastPos = line.find(s1, lastPos)) != std::string::npos) {
             line = line.substr(0, lastPos) + s2 + line.substr(lastPos + s1.length());
             lastPos += s2.length();
         }
-        // cout in outfile
         outfile << line << std::endl;
     }
     infile.close();
