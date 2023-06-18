79f2c73835786c012f47a9e37578bef58b3fb1d1
Jacob L <jacob@MacBook-Air-de-Jacob.local>
2023-06-17 21:15:24 -0400

    finish?

--- CPP Module 05/ex02/main.cpp
+++ CPP Module 05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jalevesq <jalevesq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 16:24:37 by jalevesq          #+#    #+#             */
-/*   Updated: 2023/06/17 17:06:20 by jalevesq         ###   ########.fr       */
+/*   Updated: 2023/06/17 21:14:59 by jalevesq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,25 +40,26 @@ int main(void) {
 	cout << endl;
 
 	// Test 1 - Robotomy Form
-	// Toto.signForm(robotomy);
-	// Toto.executeForm(robotomy);
-	// Johnny.signForm(robotomy);
-	// Toto.executeForm(robotomy);
-	// Johnny.executeForm(robotomy);
-
+	Toto.signForm(robotomy);
+	Toto.executeForm(robotomy);
+	Johnny.signForm(robotomy);
+	Toto.executeForm(robotomy);
+	Johnny.executeForm(robotomy);
+	cout << endl;
 	// Test 2 - President Form
-	// Toto.signForm(president);
-	// Toto.executeForm(president);
-	// Johnny.signForm(president);
-	// Toto.executeForm(president);
-	// Johnny.executeForm(president);
-
+	Toto.signForm(president);
+	Toto.executeForm(president);
+	Johnny.signForm(president);
+	Toto.executeForm(president);
+	Johnny.executeForm(president);
+	Johnny.executeForm(president);
+	cout << endl;
 	// Test 3 - ShrubberyCreation Form
 	Toto.signForm(tree);
 	Toto.executeForm(tree);
 	Tom.signForm(tree);
+	Tom.signForm(tree);
 	Toto.executeForm(tree);
-	Tom.executeForm(tree);
 	Tom.executeForm(tree);
 	
 }
