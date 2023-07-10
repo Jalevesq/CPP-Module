1e5ce3c08edd907ec5b59972d2e091afb586a713
Jacob L <jacob@MacBook-Air-de-Jacob.local>
2023-07-10 18:01:27 -0400

    finish ex00

--- CPP Module 08/ex00/easyfind.hpp
+++ CPP Module 08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: jalevesq <jalevesq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 13:26:23 by jalevesq          #+#    #+#             */
-/*   Updated: 2023/06/26 16:04:48 by jalevesq         ###   ########.fr       */
+/*   Updated: 2023/07/10 18:01:09 by jalevesq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,11 @@ using std::string;
 template<typename T>
 int	easyfind(std::vector<T> param1, int find) {
 	if (sizeof(int) != sizeof(T))
-		return (INT_MIN);
+		throw std::logic_error("Type mismatch");
+	// Typename est obligé à cause du <T> sinon ça ne compile pas. Le typename permet au compilateur de comprendre que c'est bel et bien une variable et non autre chose.
 	typename std::vector<T>::iterator iter;
-	for (typename std::vector<T>::iterator iter = param1.begin(); iter != param1.end(); iter++)
+	for (iter = param1.begin(); iter != param1.end(); iter++)
 		if (*iter == find)
 			return (find);
-	throw (std::exception());
+	throw std::runtime_error("Element not found");
 }
--- CPP Module 08/ex00/main.cpp
+++ CPP Module 08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jalevesq <jalevesq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 13:26:21 by jalevesq          #+#    #+#             */
-/*   Updated: 2023/06/26 16:04:55 by jalevesq         ###   ########.fr       */
+/*   Updated: 2023/07/10 17:59:37 by jalevesq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ int main(void)
 	std::vector<int> abc;
 	for (int i = 0; i < 30; i += 3)
 		abc.push_back(i);
-	cout << easyfind(abc, 30) << endl;
+	cout << easyfind(abc, 28) << endl;
 }
